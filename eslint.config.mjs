fe8945ebe222aae6795839163ea448d7156341d3
Maksym Shevchuk <maksymshevchuk89@gmai.com>
2025-05-16 19:07:31 +0200

    Initial setup: updated ESLint, added .gitignore and Prettier config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,21 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import eslint from '@eslint/js';
+import globals from 'globals';
 
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
-]);
+export default [
+  {
+    ignores: ['node_modules/**'],
+  },
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+    },
+    rules: {
+      ...eslint.configs.recommended.rules,
+    },
+  },
+];
